43dabd4454fb8cb9cc05b53711f025df013b9634
jorgeadammo <jorgeadammo@gmail.com>
2023-08-01 17:29:19 -0300

    fix banca put spec test

--- spec/put_banca_spec.rb
+++ spec/put_banca_spec.rb
@@ -10,11 +10,12 @@ describe 'agrupando os metodos de verbo PUT' do
         }
         @updated_banca = JSON.generate(updated_banca)
 
-        @request = Banca.put('/api-cnc/banca/157',
+        @request = Banca.put('/api-cnc/banca/30',
             :body => @updated_banca,
             :headers => {'Content-Type' => 'application/json'}
         )
         expect(@request.code).to eql(200)
+        #expect(@request.code).to eql(500)
         expect(@request['nome']).to eq(updated_banca[:nome])
         expect(@request['apelido']).to eq(updated_banca[:apelido])
         expect(@request['telefoneFixo']).to eq(updated_banca[:telefoneFixo])
